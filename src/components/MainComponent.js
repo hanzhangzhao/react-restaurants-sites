ed57dcea4245a92305df6048689687bb9e312bd8
han <hazenzhao@gmail.com>
2019-09-11 16:17:39 -0500

    Single Page Applications p1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishDetailComponent';
 import Header from './HearderComponent';
 import Footer from './FooterComponent'
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 
@@ -15,6 +19,9 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS,
       // selectedDish: null
     };
     console.log('Menu Component constructor invoked.')
@@ -28,33 +35,36 @@ class Main extends Component {
   //   this.setState({ selectedDish: dishId });
   // }
 
-render() {
+  render() {
 
-  const HomePage = () => {
-    return(
-      <Home />
-    )
-  }
+    const HomePage = () => {
+      return (
+        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+            promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+            leader={this.state.leaders.filter((leader) => leader.featured)[0]}/>
+      )
+    }
 
-  return (
-    <div>
-      {/* <Navbar dark color="primary">
+    return (
+      <div>
+        {/* <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar> */}
-      <Header />
-      {/* <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
+        <Header />
+        {/* <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
       <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} /> */}
-      <Switch>
-        <Route path="/home" component={HomePage} />
-        <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />    {/* pass in a props to the menu component */}
-        <Redirect to="/home" />
-      </Switch>
-      <Footer />
-    </div>
-  );
-}
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />    {/* pass in a props to the menu component */}
+          <Route exact path="/contactus" component={Contact} />
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default Main;
