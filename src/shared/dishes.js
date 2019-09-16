6e621b91770dcbe7623f1bac30cd2a828b8d7cb4
han <hazenzhao@gmail.com>
2019-09-15 22:56:32 -0500

    update

--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -6,7 +6,7 @@ export const DISHES =
         image: '/assets/images/uthappizza.png',
         category: 'mains',
         label:'Hot',
-        price:'4.99',
+        price:'5.99',
         featured: true,
         description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                    
         },
